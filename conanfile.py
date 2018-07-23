44123bef56adf0131d8f3c971c0b757f80e2b9e9
dandielo <daniel.penkala@gmail.com>
2018-07-23 12:52:28 +0200

    Add support for multi-config packages.

--- conanfile.py
+++ conanfile.py
@@ -22,10 +22,28 @@ end
 
 
 class PremakeDeps(object):
-    def __init__(self, deps_cpp_info):
-        self.includedirs = deps_cpp_info.include_paths
-        self.libdirs = deps_cpp_info.lib_paths
-        self.links = deps_cpp_info.libs
+    def __init__(self, source, base=None):
+        def get_unique(name):
+            attrib = getattr(source, name, [])
+            attrib_base = getattr(base, name, [])
+
+            if base == None:
+                return attrib
+
+            if type(attrib) != list:
+                return attrib
+
+            return [item for item in attrib if item not in attrib_base]
+
+        self.includedirs = get_unique('include_paths')
+        self.libdirs = get_unique('lib_paths')
+        self.links = get_unique('libs')
+
+        if base == None:
+            if hasattr(source, 'debug'):
+                self.debug = PremakeDeps(source.debug, base=source)
+            if hasattr(source, 'release'):
+                self.release = PremakeDeps(source.release, base=source)
 
     def has_cpp_info(self):
         return (self.includedirs
@@ -45,23 +63,48 @@ class PremakeModule(object):
         else:
             self.lines.append("%s" % str)
 
-    def build_property(self, name, values, paths=False):
+    def build_property(self, name, values, paths=False, IndentLevel=0):
+        indent_prop = ''.join(["    "] * IndentLevel)
+        indent_prop_arg = ''.join(["    "] * (IndentLevel + 1))
         lines = []
+
         if values:
-            lines.append(name + "{")
+            lines.append("%s%s{" % (indent_prop, name))
             for value in values:
                 if paths:
                     value = normpath(value).replace("\\", "/")
-                lines.append('    "%s",' % value)
-            lines.append("}")
+                lines.append('%s"%s",' % (indent_prop_arg, value))
+            lines.append("%s}" % indent_prop)
+
         return lines
+
+    def build_property_group(self, deps, filter=None):
+
+        indentation = 0
+        if filter != None:
+            indentation = 1
+
+        lines = []
+        lines += self.build_property("includedirs", deps.includedirs, True, IndentLevel=indentation)
+        lines += self.build_property("libdirs", deps.libdirs, True, IndentLevel=indentation)
+        lines += self.build_property("links", deps.links)
+
+        if lines and filter != None:
+            lines.insert(0, 'filter { "%s" }' % '", "'.join(filter))
+            lines.append('filter { "*" }')
+
+        return lines
+
+
 
     def build_conan_module(self, deps):
 
         lines = []
-        lines += self.build_property("includedirs", deps.includedirs, True)
-        lines += self.build_property("libdirs", deps.libdirs, True)
-        lines += self.build_property("links", deps.links)
+        lines += self.build_property_group(deps)
+        if deps.debug:
+            lines += self.build_property_group(deps.debug, [ "Debug" ])
+        if deps.release:
+            lines += self.build_property_group(deps.release, [ "Release" ])
 
         if lines:
             funcs = "\n    ".join(lines)
